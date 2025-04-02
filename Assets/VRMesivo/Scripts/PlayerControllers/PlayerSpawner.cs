92e930a5dfc0b35c38e5f67a5c6223076b4a1bbb
Anastasiia <holubieva.a.a@gmail.com>
2025-04-02 18:10:19 +0300

    Spawned VR object to main scene with button "Ready"

--- Assets/VRMesivo/Scripts/PlayerControllers/PlayerSpawner.cs
+++ Assets/VRMesivo/Scripts/PlayerControllers/PlayerSpawner.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private Vector3 _position = new Vector3(-13.8f, 7.7f, 149f);
 
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(_playerPrefab, new Vector3(1, 0, 5), Quaternion.identity);
+            Runner.Spawn(_playerPrefab, _position, Quaternion.identity);
         }
     }
 }
